d4c84fa90339effdd6cafa6b6691da9e3789a16e
Tyler Baker <146883261+ty-baker@users.noreply.github.com>
2023-10-15 15:20:22 -0700

    Add files via upload
    Prompt for Coding Journal 1 session 1

---
+++ session-1.py
@@ -0,0 +1,3 @@
+print("My name is Tyler Baker and I prefer she/they pronouns")
+print("My favorite movie is either Twilight or How to Train Your Dragon.")
+print("Sushi is my favorite food, but I also like pasta!")
